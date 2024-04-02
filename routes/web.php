3490c063e9abe33624ec90fe30882dd72725656a
Gwyn <skythekid211@gmail.com>
2024-04-02 10:13:55 +0800

    web

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\ArtworkController;
 use App\Http\Controllers\MaterialController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\VerificationController;
+use App\Http\Controllers\ChartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -59,10 +60,6 @@ Route::get('/showcase', function () {
 Route::get('/checkout', function () {
     return view('cart.checkout');
 });
-
-Route::get('/charts', function () {
-    return view('charts.charts');
-});
 // Route::get('/mail', function () {
 //     Mail::to('eliso@gmail.com')->send(new Verification());
 // });
@@ -75,14 +72,26 @@ Route::get('cusregister', [AuthController::class, 'customerRegister'])->name('cu
 Route::post('cusregister', [AuthController::class, 'cusRegister'])->name('cusregister.store');
 
 //Events
-Route::get('/events/dashboard', [EventController::class, 'dashboard'])->name('event.dashboard');
-Route::get('/events/index', [EventController::class, 'index'])->name('event.index');
+Route::get('/event/dashboard', [EventController::class, 'dashboard'])->name('event.dashboard');
+Route::get('/event/index', [EventController::class, 'index'])->name('event.index');
 Route::get('/events/create', [EventController::class, 'create'])->name('event.create');
 Route::post('/events', [EventController::class, 'store'])->name('event.store');
 Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('event.edit');
 Route::put('/events/{event}', [EventController::class, 'update'])->name('event.update');
 Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('event.destroy');
 Route::put('events/{id}/restore', [EventController::class, 'restore'])->name('event.restore');
+
+//BlogsCrud
+Route::get('/blog/dashboard', [BlogController::class, 'dashboard'])->name('blogs.dashboard');
+Route::get('/blog/index', [BlogController::class, 'index'])->name('blogs.index');
+Route::get('/blog/create', [BlogController::class, 'create'])->name('blogs.create');
+Route::post('/blogs', [BlogController::class, 'store'])->name('blogs.store');
+Route::get('/blogs/{id}/edit', [BlogController::class, 'edit'])->name('blogs.edit');
+Route::put('/blogs/{id}', [BlogController::class, 'update'])->name('blogs.update');
+Route::delete('/destroy/{id}', [BlogController::class, 'destroy'])->name('blogs.destroy');
+Route::put('/blogs/{id}/restore', [BlogController::class, 'restore'])->name('blogs.restore');
+
+
 
 //ArtworkCrud
 Route::get('/artwork/index', [ArtworkController::class, 'index'])->name('artwork.index');
@@ -115,6 +124,7 @@ Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update')
 Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 Route::put('/user/{id}/restore', [UserController::class, 'restore'])->name('user.restore');
 
+
 //CartCrud
 Route::post('/cart/store', [CartController::class, 'store'])->name('cart.store');
 
@@ -134,25 +144,5 @@ Route::get('verify/{token}', [AuthController::class, 'verify']);
 Route::get('/email/verify', [VerificationController::class, 'sendVerificationEmail'])->name('verification.send');
 Route::get('/email/verify/{id}/{hash}', [VerificationController::class, 'verify'])->middleware(['signed'])->name('verification.verify');
 
-
-//blogs route pare
-// Route to display the blogs dashboard
-Route::get('/blogsdashboard', [BlogController::class, 'dashboard'])->name('blogsdashboard');
-Route::get('/createblogs', [BlogController::class, 'create'])->name('blogs.create');
-Route::post('/createblogs', [BlogController::class, 'store'])->name('blogs.store');
-Route::get('/show/{id}', [BlogController::class, 'show'])->name('blogs.show');
-// Route::get('/edit/{id}', [BlogController::class, 'edit'])->name('blogs.edit');
-Route::delete('/destroy/{id}', [BlogController::class, 'destroy'])->name('blogs.destroy');
-// Route::get('/blogs/{id}', [BlogController::class, 'show'])->name('blogs.show');
-Route::get('/blogs/{id}/edit', [BlogController::class, 'edit'])->name('blogs.edit');
-Route::get('/blogs/{id}', [BlogController::class, 'update'])->name('blogs.update');
-// Route::delete('/blogs/{id}', [BlogController::class, 'destroy'])->name('blogs.destroy');
-
-
 //charts
-Route::get('/chart', function () {
-    $customerCount = User::where('roles', 'customer')->count();
-    $artistCount = User::where('roles', 'artist')->count();
-
-    return view('charts.charts', compact('customerCount', 'artistCount'));
-});
+Route::get('/chart', [ChartController::class, 'index']);
